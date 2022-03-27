d8fbb4ac5cbd0766d88187cf62c5c00ef187c028
jillannette <jillarnold2913@gmail.com>
2022-03-27 10:07:24 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-const findSum = ([2, 4, 6]); 
-const initialValue = 0;
+const nums = ([2, 4, 6]); 
 
-const totalSum = findSum.reduce(
-  (previousValue, currentValue) => previousValue + currentValue,
-  initialValue
-);
-
-console.log(totalSum);
+const findSum = nums.reduce((acc, item) => acc + item);
+console.log(findSum);
 // 12
