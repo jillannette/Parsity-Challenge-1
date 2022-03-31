e50a11631df3549fe4b18d259372626b014334fb
jillannette <jillarnold2913@gmail.com>
2022-03-31 13:40:39 -0400

    Update Challenge 1.js

--- Challenge 1.js
+++ Challenge 1.js
@@ -1,7 +1,6 @@
-const numbers = [2, 4, 6]; 
-
-function findSum () {
-  let sum = 0;
-    sum = numbers.reduce((acc, number) => acc + number);
+const findSum = function(array) {
+    let sum = 0;
+    sum = array.reduce((acc, number) => acc + number);
   return sum;
 };
+console.log(findSum(([2, 4, 6])));
